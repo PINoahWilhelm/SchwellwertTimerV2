d38a081328beb745c15a9d08063b39ee17d3aaeb
DESKTOP-H43QJSR\Noah <n.wilhelm@prointernet.de>
2018-07-26 20:35:32 +0200

    - Grundaufbau

---
+++ SymconTest/module.php
@@ -0,0 +1,75 @@
+<?
+require(__DIR__ . "\\pimodule.php");
+
+    // Klassendefinition
+    class SymconTest extends PISymconModule {
+
+        public $Details = true;
+
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+
+            // Selbsterstellter Code
+        }
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+
+            parent::Create();
+
+ 
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+           
+            parent::ApplyChanges();
+
+        }
+
+        public function configForm () {
+
+
+        }
+
+        protected function setExcluded() {
+
+            return array($this->AutomatikVar, $this->SperreVar);
+
+        }
+
+        public function CheckVariables () {
+
+            $switches = $this->createSwitches(array("Automatik||false", "Sperre||false", "Status||false"));
+
+        }
+
+        public function CheckScripts () {
+
+            // Scripts checken -und erstellen
+
+
+        }
+
+        public function RegisterProperties () {
+
+
+        }
+
+        public function onSperreChange () {
+
+            echo "Sperre changed :)";
+
+        }
+
+        public function onAutomatikChange () {
+
+            echo "Automatik changed :)";
+
+        }
+}
+
+?>
