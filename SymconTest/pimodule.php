a372341a1a7369c9039edf039dcf94504622e88d
DESKTOP-H43QJSR\Noah <n.wilhelm@prointernet.de>
2018-07-26 21:47:27 +0200

    no message

--- SymconTest/pimodule.php
+++ SymconTest/pimodule.php
@@ -142,7 +142,7 @@ abstract class PISymconModule extends IPSModule {
 
                 if ($this->doesExist($this->searchObjectByName($var))) {
 
-                    $this->$$var = $this->searchObjectByName($var);
+                    $this->$var = $this->searchObjectByName($var);
 
                 }
 
