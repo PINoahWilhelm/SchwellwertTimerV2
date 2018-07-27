78331a48616d60bfca307baf24e3a06e80793765
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-27 11:21:54 +0200

    no message

--- SymconSchwellwertTimerV2/module.php
+++ SymconSchwellwertTimerV2/module.php
@@ -377,6 +377,7 @@ require(__DIR__ . "\\pimodule.php");
             if ($tresholdVal == 5) {
 
                 $profile = $this->getVarProfile($source);
+                echo "Profile of " . $source . " is " . $profile;
                 $this->addProfile($tresholdVar, $profile);
 
             }
