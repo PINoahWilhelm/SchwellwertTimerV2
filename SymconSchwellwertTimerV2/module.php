d323be84e3801c84c0f19b342998bcdfa324d73e
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-08-23 10:28:42 +0200

    no message

--- SymconSchwellwertTimerV2/module.php
+++ SymconSchwellwertTimerV2/module.php
@@ -66,7 +66,7 @@ require(__DIR__ . "\\pimodule.php");
             }
 
         }
-
+ 
         protected function createBaseScript () {
             
             $sperre = $this->searchObjectByName("Sperre");
