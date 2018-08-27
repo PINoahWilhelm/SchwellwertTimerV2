8440c18be4a220b7be46474053679cf02d1b5870
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-08-27 12:31:23 +0200

    no message

--- SymconSchwellwertTimerV2/module.php
+++ SymconSchwellwertTimerV2/module.php
@@ -910,21 +910,27 @@ require(__DIR__ . "\\pimodule.php");
 
             SetValue($this->searchObjectByName("Nachlauf aktiv"), false);
 
-            if ($mode == 1) {
+            // if ($mode == 1) {
 
-                if ($statusVal != false) {
+            //     if ($statusVal != false) {
 
-                    SetValue($this->searchObjectByName("Status"), false);
+            //         SetValue($this->searchObjectByName("Status"), false);
     
-                }
+            //     }
 
-            } else if ($mode == 2) {
+            // } else if ($mode == 2) {
 
-                if ($statusVal != false) {
+            //     if ($statusVal != false) {
 
-                    SetValue($this->searchObjectByName("Status"), false);
+            //         SetValue($this->searchObjectByName("Status"), false);
     
-                }
+            //     }
+
+            // }
+
+            if ($statusVal != false) {
+
+                SetValue($this->searchObjectByName("Status"), false);
 
             }
 
